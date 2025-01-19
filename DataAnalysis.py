f36cff0c1d25d74d663dd867569e5e473d786263
itsMohitShah <mohit.shah165@gmail.com>
2025-01-19 16:32:59 +0100

    modularize data analysis functions for better organization

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -18,54 +18,75 @@ colors = [
     (175, 52, 45),      # Brown
     (255, 255, 255)     # White
 ]
-mask_FW = FileWalker('dl_challenge','mask.npy')
-bbox3d_FW = FileWalker('dl_challenge','bbox3d.npy')
-pc_FW = FileWalker('dl_challenge','pc.npy')
-rgb_FW = FileWalker('dl_challenge','rgb.jpg')
 
-path_imgwithmask = 'ImageswithSegmentationMask'
-os.makedirs(path_imgwithmask,exist_ok=True)
 
-dict_numofinstances = {}
 
-for idx, mask in tqdm(enumerate(mask_FW.id),total=len(mask_FW.id)):
-    mask = np.load(mask_FW.roots[idx])
-    bbox3d = np.load(bbox3d_FW.roots[idx])
-    pc = np.load(pc_FW.roots[idx])
-    rgb = plt.imread(rgb_FW.roots[idx])
 
-    print(mask.shape)
-    print(bbox3d.shape)
-    print(pc.shape)
-    print(rgb.shape)
-    numofinstances = mask.shape[0]
-    try:
-        dict_numofinstances[numofinstances] += 1
-    except:
-        dict_numofinstances[numofinstances] = 1
-    mask_img = np.zeros_like(rgb)
+def inspect_segmentationmask():
+    dict_numofinstances = {}
+    path_imgwithmask = 'ImageswithSegmentationMask'
+    os.makedirs(path_imgwithmask,exist_ok=True)
+    for idx, _ in tqdm(enumerate(mask_FW.id),total=len(mask_FW.id)):
+        mask = np.load(mask_FW.roots[idx])
+        rgb = plt.imread(rgb_FW.roots[idx])
 
-    for dim in range(mask.shape[0]):
-        x = np.where(mask[dim]==True)
-        mask_img[x] = colors[dim%len(colors)]
+        # print(mask.shape)
+        # print(bbox3d.shape)
+        # print(pc.shape)
+        # print(rgb.shape)
+        numofinstances = mask.shape[0]
+        try:
+            dict_numofinstances[numofinstances] += 1
+        except:
+            dict_numofinstances[numofinstances] = 1
+        mask_img = np.zeros_like(rgb)
 
-    plt.figure(figsize=(15,15))
-    plt.subplot(2,1,1)
-    plt.imshow(rgb)
-    plt.axis('off')
-    plt.subplot(2,1,2)
-    plt.imshow(mask_img)
-    plt.text(40,40,f'Number of instances: {numofinstances}',color='white',fontsize=15, backgroundcolor='gray')
-    plt.axis('off')
-    plt.savefig(os.path.join(path_imgwithmask,f'rgbmask_{idx}--{numofinstances}instances.png'), bbox_inches='tight')
-    plt.close()
-    
-print(dict_numofinstances)
-bars = plt.bar(dict_numofinstances.keys(),dict_numofinstances.values(), edgecolor='white')
-plt.title("Distribution of Number of Instances")
-plt.xlabel("Number of instances in an image")
-plt.ylabel("Number of occurences")
-plt.bar_label(bars, fontsize=10, color='black')
-plt.savefig('Distribution_of_Number_of_Instances.png')
-print("Execution complete")
+        for dim in range(mask.shape[0]):
+            x = np.where(mask[dim]==True)
+            mask_img[x] = colors[dim%len(colors)]
+
+        # plt.figure(figsize=(15,15))
+        # plt.subplot(2,1,1)
+        # plt.imshow(rgb)
+        # plt.axis('off')
+        # plt.subplot(2,1,2)
+        # plt.imshow(mask_img)
+        # plt.text(40,40,f'Number of instances: {numofinstances}',color='white',fontsize=15, backgroundcolor='gray')
+        # plt.axis('off')
+        # plt.savefig(os.path.join(path_imgwithmask,f'rgbmask_{idx}--{numofinstances}instances.png'), bbox_inches='tight')
+        # plt.close()
+        
+    # print(dict_numofinstances)
+    bars = plt.bar(dict_numofinstances.keys(),dict_numofinstances.values(), edgecolor='white')
+    plt.title("Distribution of Number of Instances")
+    plt.xlabel("Number of instances in an image")
+    plt.ylabel("Number of occurences")
+    plt.bar_label(bars, fontsize=10, color='black')
+    plt.savefig('Distribution_of_Number_of_Instances.png')
+    print("Execution complete")
+
+
+
+def inspect_bbox3d():
+    for idx, _ in tqdm(enumerate(mask_FW.id),total=len(mask_FW.id)):
+        bbox3d = np.load(bbox3d_FW.roots[idx])
+        print(bbox3d.shape)
+        # print(bbox3d)
+        break
+    print("Execution complete")
+
+
+def inspect_pc():
+    for idx, _ in tqdm(enumerate(pc_FW.id),total=len(pc_FW.id)):
+        pc = np.load(pc_FW.roots[idx])
+
+
+if __name__ == "__main__":
+    mask_FW = FileWalker('dl_challenge','mask.npy') # Segmentation Mask (number of instances, H, W)
+    bbox3d_FW = FileWalker('dl_challenge','bbox3d.npy')
+    pc_FW = FileWalker('dl_challenge','pc.npy')
+    rgb_FW = FileWalker('dl_challenge','rgb.jpg')
+
+    # inspect_segmentationmask()
+    inspect_bbox3d()
 
