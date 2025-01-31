b0f3fc3863e389c80a16538b61411378053040e8
itsMohitShah <mohit.shah165@gmail.com>
2025-01-31 02:21:01 +0100

    add point cloud visualization utility using Open3D

---
+++ Utils/Open3D_PointCloudVisualizer.py
@@ -0,0 +1,16 @@
+# Having an easy way to visualize point clouds. Open a file dialogue and navigate to the .ply file.
+#TODO: Plot BBoxs as well
+
+import open3d as o3d
+import tkinter as tk
+from tkinter import filedialog
+import os
+
+while True:
+    window = tk.Tk()
+    window.withdraw()
+    path = tk.filedialog.askopenfilename()
+    if path == "":
+        break
+    pcd = o3d.io.read_point_cloud(path)
+    o3d.visualization.draw_geometries([pcd])
